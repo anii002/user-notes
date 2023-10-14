e52dbfa3738579de3c065e08413e1817ce4e479a
unknown <aniket102002@gmial.com>
2023-10-14 22:29:13 +0530

    import path

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // import express from "express";
 const mongoose = require("mongoose");
-
+const path = require("path")
 const cors = require("cors");
 require("dotenv").config();
 // set up express
